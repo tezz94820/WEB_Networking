4b3fcfc322c39cff82cbb85679812e548042d7d5
Tejas Itankar <tejasitankar94820@gmail.com>
2023-08-21 14:24:16 +0530

    node event loop

---
+++ 13-redis/Implement/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const redis = require('redis');
+
+const client = redis.createClient(6379);
+
+const cache = (req, res, next) => {
+    const { username } = req.params; // Extract the username from req.params
+    client.get(username, (err, data) => {
+        if (err) throw err;
+        if (data !== null) {
+            res.send(`<h1>${username} has ${data} public github repos (cached)</h1>`);
+        } else {
+            next(); // Continue to the next middleware if data is not cached
+        }
+    }); 
+};
+
+const getusername = async (req, res) => {
+    const {username} = req.params;
+    console.log('hitting the server for '+username);
+    const response = await axios.get('https://api.github.com/users/'+username);
+    const public_repos = response.data.public_repos;
+    if(public_repos !=null || public_repos != undefined){
+        client.set(username,public_repos);
+    }
+    res.send(`<h1>${username} has ${public_repos} public github repos</h1>`);
+}
+app.get('/getcontent/:username', cache , getusername );
+
+app.listen(5000 , () => {
+    console.log('server is listening on port 5000')
+}) 
